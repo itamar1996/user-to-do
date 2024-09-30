0ff3c96ea9d34c503d81381d96660bc0eeea9032
itamar1996 <itay9919@gmail.com>
2024-09-30 11:11:37 +0300

    controllers empty

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, {Express} from 'express'
 import cookieParser from "cookie-parser"
 import 'dotenv/config'
+import authController from './controllers/authcontroller'
+import userController from './controllers/usercontroller'
+import todoController from './controllers/todocontroller'
 
+// import 
 const app:Express = express()
 
 app.use(express.json()) // to get the req.body
 app.use(cookieParser()) // to get the req.cookie(s)
+app.use('/auth',authController)
+app.use('/todos',todoController)
+app.use('/users',userController)
 
 app.listen(process.env.port, () => console.log(
     `Server is up and running, feel free to visit at http://localhost:${process.env.PORT}`
